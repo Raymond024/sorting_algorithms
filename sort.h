0ff3273ded25771a58b33bceb96b5e0889b2a852
Raymond024 <raymondmugimba2@gmail.com>
2024-05-04 13:36:52 -0700

    1

--- sort.h
+++ sort.h
@@ -39,5 +39,7 @@ void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
 void swap_integers(int *x, int *y);
+void swaps_nodes(listint_t **head, listint_t **node1, listint_t *node2);
+void insertion_sort_list(listint_t **list);
 
 #endif
